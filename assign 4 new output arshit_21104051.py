63a595b129ea79383d45a5c6fcfd53fa665fcd0c
rsht7 <96733088+rsht7@users.noreply.github.com>
2022-03-01 20:26:53 +0530

    Add files via upload

---
+++ assign 4 new output arshit_21104051.py
@@ -0,0 +1,101 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: G:\PYTHON\assign 4 code.py =====================
+Ques 1 
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+
+Ques 2 
+ 
+Enter number of rows to be in Pascal's Triangle: 7
+
+Using recursions:
+
+            1   
+          1   1   
+        1   2   1   
+      1   3   3   1   
+    1   4   6   4   1   
+  1   5   10   10   5   1   
+1   6   15   20   15   6   1   
+
+Using loops,(iterations):
+
+            1   
+          1   1   
+        1   2   1   
+      1   3   3   1   
+    1   4   6   4   1   
+  1   5   10   10   5   1   
+1   6   15   20   15   6   1   
+
+
+
+Ques 3
+
+Enter the first integer: 7
+Enter the second integer: 5
+The remainder is :  2
+The quotient is :  1
+a) Checking if the quotient and remainder is a callable value or not.
+False
+False
+
+b) Checking for zeroes
+Both the values are non zero
+
+c) and d)
+{6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+
+e)
+Immutable set:  frozenset({8, 5, 6, 7})
+
+f)
+Largest value in the set:  8
+Hash value:  8
+
+
+
+Ques 4
+
+Object created
+The name of the student is Arshit and roll number is 21104051.
+Object destroyed
+
+
+
+Ques 5
+ 
+Part a) The updated salary of the employee Mehak is 70000
+Part b) Viren's record has been deleted Employee Viren record deleted
+
+
+
+Ques 6
+ 
+Hey 1st friend, you may enter a word: sent
+
+Dear 2nd friend, now you shall enter a new meaningful word using only the letters of previously entered word to test your friendship: nest
+
+Does the word make sense?(y or n)
+y
+The friends have passed the friendship test !
+
+
+
+
+Thank You
